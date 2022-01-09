202fa118de5c02dd788f299872208b00cb4b5b2a
Miguel Colom <miguel@mcolom.info>
2022-01-09 10:48:18 +0100

    Added moths.asm

--- dis/joint.asm
+++ dis/joint.asm
@@ -166,6 +166,10 @@ include 'enemies.asm'
 ; Magical elements, mainly at the beginning of level #2
 include 'magical_elements.asm'
 
+; The moths at level #4
+include 'moths.asm'
+
+
 ; The head of the magician. Creepy!
 TBL_HEAD: EQU 0xE31B
 
@@ -9198,7 +9202,7 @@ l3813h:
 	add hl,bc			;382f	09 	. 
 	sbc hl,de		;3830	ed 52 	. R 
 	jp nc,l38d9h		;3832	d2 d9 38 	. . 8 
-	ld hl,0e576h		;3835	21 76 e5 	! v . 
+	ld hl,TBL_MOTHS		;3835	21 76 e5 	! v . 
 	ld a,(0e50bh)		;3838	3a 0b e5 	: . . 
 	cp (hl)			;383b	be 	. 
 	jp c,l38d9h		;383c	da d9 38 	. . 8 
@@ -9441,8 +9445,9 @@ l39b9h:
 	djnz l396ah		;39bd	10 ab 	. . 
 	ret			;39bf	c9 	. 
 
+; Adding a RET (C9) here avoids that the moths at level #4 appear.
 sub_39c0h:
-	ld a,(0e576h)		;39c0	3a 76 e5 	: v . 
+	ld a,(TBL_MOTHS)		;39c0	3a 76 e5 	: v . 
 	and a			;39c3	a7 	. 
 	ret z			;39c4	c8 	. 
 	ld ix,TBL_WIDTH_21		;39c5	dd 21 77 e5 	. ! w . 
@@ -9837,12 +9842,12 @@ sub_3ccfh:
 	ret			;3cf1	c9 	. 
 
 l3cf2h:
-	ld hl,0e576h		;3cf2	21 76 e5 	! v . 
+	ld hl,TBL_MOTHS		;3cf2	21 76 e5 	! v . 
 	dec (hl)			;3cf5	35 	5 
 	ld (ix + 0),000h		;3cf6	dd 36 00 00 	. 6 . . 
 	ret			;3cfa	c9 	. 
 l3cfbh:
-	ld hl,0e576h		;3cfb	21 76 e5 	! v . 
+	ld hl,TBL_MOTHS		;3cfb	21 76 e5 	! v . 
 	dec (hl)			;3cfe	35 	5 
 	bit 6,(ix + 0)		;3cff	dd cb 00 76 	. . . v 
 	ld (ix + 0),000h		;3d03	dd 36 00 00 	. 6 . . 
@@ -9902,7 +9907,7 @@ sub_3d55h:
 sub_3d59h:
 	ld b,050h		;3d59	06 50 	. P 
 l3d5bh:
-	ld hl,0e576h		;3d5b	21 76 e5 	! v . 
+	ld hl,TBL_MOTHS		;3d5b	21 76 e5 	! v . 
 	ld a,(0e50bh)		;3d5e	3a 0b e5 	: . . 
 	cp (hl)			;3d61	be 	. 
 	ret c			;3d62	d8 	. 
---
+++ dis/moths.asm
@@ -0,0 +1,3 @@
+; The moths at level #4
+
+TBL_MOTHS: EQU 0xE576
